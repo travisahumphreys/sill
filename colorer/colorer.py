580fdba8a627e6dd18264be7aca9fd166ec24b83
travis h <travis.a.humphreys@gmail.com>
2026-03-25 14:04:28 -0500

    edited  to use a dict comprehension instead of loop

--- colorer/colorer.py
+++ colorer/colorer.py
@@ -30,10 +30,13 @@ def is_hex(color):
 
 
 def hex_to_rgb(palette):
-    palette_rgb = {}
-    for key in palette:
-        rgb = [int(palette[key][index : index + 2], 16) for index in range(1, 7, 2)]
-        palette_rgb[key] = rgb
+    palette_rgb = {
+        key: [int(value[index : index + 2], 16) for index in range(1, 7, 2)]
+        for key, value in palette.items()
+    }
+    # for key in palette:
+    #     rgb = [int(palette[key][index : index + 2], 16) for index in range(1, 7, 2)]
+    #     palette_rgb[key] = rgb
     return palette_rgb
 
 
